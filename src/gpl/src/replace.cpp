9159e593b1360c3fe37663821199036761cc6377
Jannis Schönleber <joennlae@gmail.com>
2023-06-20 00:49:11 +0200

    gpl: fix PlacerBase construction calls

--- src/gpl/src/replace.cpp
+++ src/gpl/src/replace.cpp
@@ -197,12 +197,12 @@ void Replace::doIncrementalPlace()
 
     pbc_ = std::make_shared<PlacerBaseCommon>(db_, dp_, pbVars, log_);
 
-    pbVec_.push_back(std::make_shared<PlacerBase>(db_, dp_, pbc_, log_));
+    pbVec_.push_back(std::make_shared<PlacerBase>(db_, pbc_, log_));
 
     for (auto pd : db_->getChip()->getBlock()->getPowerDomains()) {
       if (pd->getGroup()) {
         pbVec_.push_back(
-            std::make_shared<PlacerBase>(db_, dp_, pbc_, log_, pd->getGroup()));
+            std::make_shared<PlacerBase>(db_, pbc_, log_, pd->getGroup()));
       }
     }
 
@@ -280,12 +280,12 @@ void Replace::doInitialPlace()
 
     pbc_ = std::make_shared<PlacerBaseCommon>(db_, dp_, pbVars, log_);
 
-    pbVec_.push_back(std::make_shared<PlacerBase>(db_, dp_, pbc_, log_));
+    pbVec_.push_back(std::make_shared<PlacerBase>(db_, pbc_, log_));
 
     for (auto pd : db_->getChip()->getBlock()->getPowerDomains()) {
       if (pd->getGroup()) {
         pbVec_.push_back(
-            std::make_shared<PlacerBase>(db_, dp_, pbc_, log_, pd->getGroup()));
+            std::make_shared<PlacerBase>(db_, pbc_, log_, pd->getGroup()));
       }
     }
 
@@ -326,12 +326,12 @@ bool Replace::initNesterovPlace()
 
     pbc_ = std::make_shared<PlacerBaseCommon>(db_, dp_, pbVars, log_);
 
-    pbVec_.push_back(std::make_shared<PlacerBase>(db_, dp_, pbc_, log_));
+    pbVec_.push_back(std::make_shared<PlacerBase>(db_, pbc_, log_));
 
     for (auto pd : db_->getChip()->getBlock()->getPowerDomains()) {
       if (pd->getGroup()) {
         pbVec_.push_back(
-            std::make_shared<PlacerBase>(db_, dp_, pbc_, log_, pd->getGroup()));
+            std::make_shared<PlacerBase>(db_, pbc_, log_, pd->getGroup()));
       }
     }
 
